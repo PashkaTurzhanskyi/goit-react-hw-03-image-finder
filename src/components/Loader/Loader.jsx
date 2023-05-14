02ef6fab69e208ae9a535ab7d2fc68e9dbc70693
PashkaTurzhanskyi <pavlo.turzhanskiy@gmail.com>
2023-05-14 18:14:30 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,10 +4,20 @@ import { Oval } from 'react-loader-spinner';
 export const Loader = () => (
   <div>
     <Oval
-      height={80}
-      width={80}
+      height={400}
+      width={400}
       color="#4fa94d"
-      wrapperStyle={{}}
+      wrapperStyle={{
+        position: 'fixed',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+      }}
       wrapperClass=""
       visible={true}
       ariaLabel="oval-loading"
